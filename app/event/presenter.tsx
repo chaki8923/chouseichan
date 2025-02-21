cc92c140536b2599e32cddada71fc425b71d033f
茶木 涼 <>
2025-02-21 15:59:03 +0900

    feat: neverUsed解消

--- app/event/presenter.tsx
+++ app/event/presenter.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Session } from "@auth/core/types";
