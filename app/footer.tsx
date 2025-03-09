3ad7c98848b0e7aa8de90a569af053b169d9b9dc
chaki8923 <aowp.contact@gmail.com>
2025-03-09 12:47:24 +0900

    feat: target_blank

--- app/footer.tsx
+++ app/footer.tsx
@@ -38,7 +38,7 @@ const Footer: React.FC = () => {
             <a href="https://x.com/choseichan2025" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
               <FaSquareXTwitter size={24} />
             </a>
-            <a href="https://github.com/chaki8923/chouseichan" className={styles.socialLink}>
+            <a href="https://github.com/chaki8923/chouseichan" target="_blank" className={styles.socialLink}>
               <FaGithub size={24} />
             </a>
           </div>
