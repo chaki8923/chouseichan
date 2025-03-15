7dbc45b265af3833de1cea246b5c728a0bf99c2d
chaki8923 <aowp.contact@gmail.com>
2025-03-15 09:15:07 +0900

    feat: 説明ページの変更

---
+++ app/description/metadata.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "調整ちゃんとは？ | 調整ちゃん",
+    description: "調整ちゃんとは、単なる日程調整にとどまらずに、Googleカレンダーとの連携や、回答期限の設定、主役設定、お店選び投票、イベント開催後の思い出画像共有機能などイベントの計画から開催後まで幹事も参加者も使い易く、楽しめる機能がたくさんあるサービスです。",
+    keywords: ["イベント", "スケジュール", "調整", "日程調整"],
+    robots: "index, follow",
+}; 
